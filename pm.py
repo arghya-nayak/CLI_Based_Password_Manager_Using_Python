c57ec7b9aa58b445f9493fcd25c47edaf5f820cd
Arghya <arghya05nayak@gmail.com>
2026-02-21 16:13:04 +0530

    Delete and Upade password feature update on cli

--- pm.py
+++ pm.py
@@ -8,11 +8,13 @@ from rich import print as printc
 import utils.add
 import utils.retrieve
 import utils.generate
+import utils.delete
+import utils.update
 from utils.dbconfig import dbconfig
 
-parser = argparse.ArgumentParser(description='Description')
+parser = argparse.ArgumentParser(description='Password Manager')
 
-parser.add_argument('option', help='(a)dd / (e)xtract / (g)enerate')
+parser.add_argument('option', help='(a)dd / (e)xtract / (g)enerate / (d)elete / (u)pdate')
 parser.add_argument("-s", "--name", help="Site name")
 parser.add_argument("-u", "--url", help="Site URL")
 parser.add_argument("-e", "--email", help="Email")
@@ -82,6 +84,64 @@ def main():
         password = utils.generate.generatePassword(args.length)
         pyperclip.copy(password)
         printc("[green][+][/green] Password generated and copied to clipboard")
+
+    if args.option in ["delete", "d"]:
+        # Require master password first
+        res = inputAndValidateMasterPassword()
+        if res is None:
+            return
+        
+        if args.name is None and args.url is None and args.email is None and args.login is None:
+            # Show all entries and delete by ID
+            printc("[cyan][*][/cyan] Listing all entries...\n")
+            all_entries = utils.delete.listEntries()
+            
+            if len(all_entries) == 0:
+                return
+            
+            try:
+                entry_id = int(input("\nEnter the ID of the entry to delete (0 to cancel): "))
+                if entry_id == 0:
+                    printc("[yellow][-][/yellow] Cancelled")
+                    return
+                
+                confirm = input(f"Are you sure you want to delete entry {entry_id}? (yes/no): ")
+                if confirm.lower() in ["yes", "y"]:
+                    utils.delete.deleteEntryById(entry_id, all_entries)
+                else:
+                    printc("[yellow][-][/yellow] Cancelled")
+            except ValueError:
+                printc("[red][!][/red] Invalid input")
+        else:
+            # Delete by search criteria
+            if args.name is None or args.url is None:
+                printc("[red][!][/red] Both Site Name (-s) and Site URL (-u) are required for targeted delete")
+                return
+            
+            if args.email is None:
+                args.email = ""
+            if args.login is None:
+                args.login = ""
+            
+            confirm = input(f"Delete entry for {args.name}? (yes/no): ")
+            if confirm.lower() in ["yes", "y"]:
+                utils.delete.deleteEntry(args.name, args.url, args.email, args.login)
+            else:
+                printc("[yellow][-][/yellow] Cancelled")
+
+    if args.option in ["update", "u"]:
+        if args.name is None or args.url is None:
+            printc("[red][!][/red] Both Site Name (-s) and Site URL (-u) are required")
+            return
+        
+        if args.email is None:
+            args.email = ""
+        if args.login is None:
+            args.login = ""
+        
+        res = inputAndValidateMasterPassword()
+        if res is not None:
+            utils.update.updateEntry(res[0], res[1], args.name, args.url, args.email, args.login)
 
 
 main()
